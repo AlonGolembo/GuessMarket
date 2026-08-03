f3c8e671ca6f97880d0e56ac5573faf85145af77
AlonGolembo <alon.golembo@gmail.com>
2026-08-03 22:47:59 +0300

    Create EventDTO

---
+++ GuessMarket.DTO/src/dto/EventDTO.java
@@ -0,0 +1,69 @@
+package dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class EventDTO implements Serializable {
+    private UUID eventId;
+    private String eventName;
+    private String eventDescription;
+    private double eventCommission;
+    private boolean isEventActive;
+    private double commissionChargedSum;
+
+    public EventDTO() {
+    }
+
+    public EventDTO(UUID eventId, String eventName, String eventDescription, double eventCommission, boolean isEventActive) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventCommission = eventCommission;
+        this.isEventActive = isEventActive;
+        this.commissionChargedSum =0;
+    }
+
+    public UUID getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(UUID eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public double getEventCommission() {
+        return eventCommission;
+    }
+
+    public void setEventCommission(double eventCommission) {
+        this.eventCommission = eventCommission;
+    }
+
+    public boolean isEventActive() {
+        return isEventActive;
+    }
+
+    public void setEventActive(boolean eventActive) {
+        isEventActive = eventActive;
+    }
+
+    public double getCommissionChargedSum() {
+        return commissionChargedSum;
+    }
+}

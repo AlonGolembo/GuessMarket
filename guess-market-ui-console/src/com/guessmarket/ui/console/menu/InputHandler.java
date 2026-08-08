6f2926c124b60623b630334afe92166581d1f981
AlonGolembo <alon.golembo@gmail.com>
2026-08-08 16:20:38 +0300

    Create InputHandler

---
+++ guess-market-ui-console/src/com/guessmarket/ui/console/menu/InputHandler.java
@@ -0,0 +1,59 @@
+package com.guessmarket.ui.console.menu;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class InputHandler {
+    private final Scanner scanner;
+    public InputHandler(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int readIntRange(String prompt, int min, int max){
+        while(true){
+            System.out.println(prompt);
+            String input = scanner.nextLine().trim();
+
+            try{
+                int choice = Integer.parseInt(input);
+                if(choice >= min && choice <= max){
+                    return choice;
+                }
+
+                System.out.println("Choice out of range! Please enter a number between" + min + " and" + max + ".");
+            } catch(NumberFormatException e){
+                System.out.println("Invalid input! Please enter a valid integer.");
+            }
+        }
+    }
+
+    public int readPositiveInt(String prompt){
+        while(true){
+            System.out.println(prompt);
+            String input = scanner.nextLine().trim();
+
+            try{
+                int value = Integer.parseInt(input);
+                if(value >= 0){
+                    return value;
+                }
+
+                System.out.println("Value must be positive! Please enter a number larger or equal to 0.");
+            } catch(NumberFormatException e){
+                System.out.println("Invalid input! Please enter a valid positive integer.");
+            }
+        }
+    }
+
+    public String readNonEmptyString(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String input = scanner.nextLine().trim();
+
+            if (!input.isEmpty()) {
+                return input;
+            }
+            System.out.println("Input cannot be empty! Please try again.");
+        }
+    }
+}
